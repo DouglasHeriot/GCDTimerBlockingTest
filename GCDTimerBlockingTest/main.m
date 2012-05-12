f08d46c2f9a529a3b2dcc4b886a70ccd8e0f2bef
Douglas Heriot <douglas@douglasheriot.com>
2012-05-12 23:41:49 +1000

    Wrote test program to see how GCD handles timers while it’s currently processing.

--- GCDTimerBlockingTest/main.m
+++ GCDTimerBlockingTest/main.m
@@ -16,6 +16,19 @@ int main(int argc, const char * argv[])
 	    // insert code here...
 	    NSLog(@"Hello, World!");
 	    
+		dispatch_queue_t queue = dispatch_queue_create("test", DISPATCH_QUEUE_SERIAL);
+		dispatch_source_t timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, queue);
+		dispatch_source_set_timer(timer, DISPATCH_TIME_NOW, 1 * NSEC_PER_SEC, 0);
+		dispatch_resume(timer);
+		dispatch_source_set_event_handler(timer, ^{
+			NSLog(@"Tick!");
+			sleep(3);
+		});
+		
+		sleep(4);
+		dispatch_suspend(timer);
+		
+		[[NSRunLoop mainRunLoop] run];
 	}
     return 0;
 }
