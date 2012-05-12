8d9587faa2496e4b6b496dd0f10472d2a2a1868d
Douglas Heriot <douglas@douglasheriot.com>
2012-05-12 22:19:30 +1000

    Initial Commit

---
+++ GCDTimerBlockingTest/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  GCDTimerBlockingTest
+//
+//  Created by Douglas Heriot on 12/05/12.
+//  Copyright (c) 2012 Douglas Heriot Design. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
